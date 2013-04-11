c3ee7dfd2c7c4ac320d40bf35122f997708fb8e5
iirvine <icirvine@gmail.com>
2013-04-11 16:32:16 -0700

    getting there...

--- src/edit/handler/LimitVertices.js
+++ src/edit/handler/LimitVertices.js
@@ -22,6 +22,10 @@ L.Polyline.addInitHook(function(){
 })
 
 L.Edit.Poly.Custom = L.Edit.Poly.extend({
+	options: {
+		hidden: false
+	},
+
 	_compose: function(){
 		if (!arguments) return;
 		var funcs = arguments;
@@ -35,11 +39,14 @@ L.Edit.Poly.Custom = L.Edit.Poly.extend({
 	},
 
 	_bindMiddleMarker: function(marker, marker1, marker2) {
-		var markers = [marker, marker1, marker2];
-		var pipeline = this._compose(this._logMarkersLength, this._doSomethingToFifthMarker, this._limitVertices, this._onMidMarkerDragStart)
+		var markers = [marker, marker1, marker2],
+			onDragStartPipe = this._compose(this._logMarkersLength, this._doSomethingToFifthMarker, this._limitVertices, this._onMidMarkerDragStart),
+			onDragEndPipe = this._compose(this._exitIfHidden, this._onMidMarkerDragEnd);
 
-		var onDragStart = this._getHandler(pipeline, markers, marker.getLatLng());
-		var onDragEnd = this._getHandler(this._onMidMarkerDragEnd, markers, onDragStart);
+
+
+		var onDragStart = this._getHandler(onDragStartPipe, markers, marker.getLatLng());
+		var onDragEnd = this._getHandler(onDragEndPipe, markers, onDragStart);
 		var onClick = this._getHandler(this._onMidMarkerClick, onDragStart, onDragEnd);
 
 		marker
@@ -48,34 +55,55 @@ L.Edit.Poly.Custom = L.Edit.Poly.extend({
 		    .on('dragend', onDragEnd, this);
 	},
 
+	_hideMiddleMarkers: function() {
+		var middleMarkers = [];
+		this._markers = [];
+		this._markerGroup.eachLayer(function(marker){
+			if (marker.options.opacity == 0.6) {
+				middleMarkers.push(marker)
+			}
+		});
+
+		middleMarkers.forEach(function(marker){ this._markerGroup.removeLayer(marker) }, this);
+		this._markerGroup.eachLayer(function(marker) { this._markers.push(marker) }, this);
+		this.hidden = true;
+	},
+
 	_doSomethingToFifthMarker: function(next) {
-		var aspect = function(markers) {
+		return function() {
 			if (this._markers.length == 5) {
 				console.log("hello from fifth marker aspec!");
 			}
 			next.call(this);
 		};
-		return aspect
+	},
+
+	_exitIfHidden: function(next) {
+		return function() {
+			if (this.hidden) {
+				return;
+			}
+			next.call(this);
+		}
 	},
 
 	_logMarkersLength: function(next) {
-		var aspect = function() {
+		return function() {
 			console.log(this._markers.length);
 			next.call(this);
 		}
-		return aspect;
 	},
 
 	_limitVertices: function(next) {
-		var aspect = function() {
+		return function() {
 			if (this._markers.length < 5) {
 				console.log('okay, good to go');
 				next.call(this);
 			} else {
 				console.log('uh oh, too many markers');
 				next.call(this);
+				this._hideMiddleMarkers();
 			}
 		}
-		return aspect;
 	}
 });
